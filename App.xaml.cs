b6ab0e5f762a5559de188419c8efa05169ed5070
Francisco Javier Calvo Dominguez <jaaviercalvo@gmail.com>
2025-01-27 17:55:17 +0100

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimerEjercicioFJCD
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimerEjercicioFJCD
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
